9299a0c21a28cba6072075cd7efc432fa7a08f25
varunpats <varunpatil2502@gmail.com>
2023-09-04 12:57:29 +0530

    Added gitignore and changed folder structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import keyConceptsImage from './assets/images/key-concepts.png';
 import componentsImage from './assets/images/components.png';
+import Header from './components/Header/Header';
+import Concept from './components/Concept/Concept';
 import stateImage from './assets/images/state.png';
 import eventsImage from './assets/images/events.png';
 
@@ -27,17 +28,11 @@ const concepts = [
 function App() {
   return (
     <div>
-      <header>
-        <img src={keyConceptsImage} alt="Medal badge with a star" />
-        <h1>Key React Concepts</h1>
-        <p>Selected key React concepts you should know about</p>
-      </header>
+      <Header />
       <ul id="concepts">
-        <li className="concept">
-          <img src="TODO: IMAGE" alt="TODO: TITLE" />
-          <h2>TODO: TITLE</h2>
-          <p>TODO: DESCRIPTION</p>
-        </li>
+        <Concept image={concepts[0].image} title={concepts[0].title} description={concepts[0].description} />
+        <Concept image={concepts[1].image} title={concepts[1].title} description={concepts[1].description} />
+        <Concept image={concepts[2].image} title={concepts[2].title} description={concepts[2].description} />
       </ul>
     </div>
   );
